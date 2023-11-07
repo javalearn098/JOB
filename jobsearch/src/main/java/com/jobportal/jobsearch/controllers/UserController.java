de3fc823a757408377ee3707d6337ce520c4a5db
unknown <vinod.javalearn@gmail.com>
2023-11-07 08:41:47 +0530

    crud operations for user

--- jobsearch/src/main/java/com/jobportal/jobsearch/controllers/UserController.java
+++ jobsearch/src/main/java/com/jobportal/jobsearch/controllers/UserController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +31,7 @@ public class UserController {
 
 	@Autowired
 	JobService jobService;
-	
+
 	@PostMapping("/save")
 	public String saveUser(@RequestBody User user) {
 
@@ -46,10 +49,29 @@ public class UserController {
 		String message = userService.login(username, password);
 		return message;
 	}
-	
+
 	@GetMapping("/jobList")
-	public List<JobDetails> getJobPostList(){
+	public List<JobDetails> getJobPostList() {
 		return jobService.getJobPostsList();
+	}
+
+	@PutMapping("/update/{id}")
+	public String updateUser(@RequestBody User user, @PathVariable(name = "id") Integer id) {
+
+		String message;
+		try {
+			message = userService.update(user, id);
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+		return message;
+	}
+
+	@DeleteMapping("/remove/{id}")
+	public String deleteuser(@PathVariable(name = "id") Integer id) {
+
+		String message = userService.delete(id);
+		return message;
 	}
 
 }
